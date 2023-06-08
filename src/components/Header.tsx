3c410a5dcf39c35d20f0445d705a3236713516fb
aleksandar-code <83082486+aleksandar-code@users.noreply.github.com>
2023-06-08 19:15:20 +0200

    Create Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,24 @@
 export function Header() {
-  return <>Header</>;
+  return (
+    <>
+      <header>
+        <h1>Memory Game</h1>
+        <div>
+          <p>
+            Get points by clicking on an image but don't click anymore than
+            once!
+          </p>
+          <div>
+            <div>
+              Current Score: <span>0</span>
+            </div>
+
+            <div>
+              Best Score: <span>7</span>
+            </div>
+          </div>
+        </div>
+      </header>
+    </>
+  );
 }
