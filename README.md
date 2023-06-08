a43122c4c843bf4bb6807cccf1ad27b5bf9f699e
aleksandar-code <83082486+aleksandar-code@users.noreply.github.com>
2023-06-08 13:49:44 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Memory Card
+
+This is a project using React JS & TypeScript.
+
+My first project using TypeScript.
---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App(): JSX.Element {
+  return (
+    <>
+      <div>hello</div>
+    </>
+  );
+}
